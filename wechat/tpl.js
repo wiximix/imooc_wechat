d8de3024f811d69d48cbd014bf5d206661a6bcd5
WinDaX <272746610@qq.com>
2016-08-10 14:45:49 +0800

    4125

--- wechat/tpl.js
+++ wechat/tpl.js
@@ -5,43 +5,43 @@ var heredoc = require('heredoc')
 
 var tpl = heredoc(function() {/*
     <xml>
-        <ToUserName><![CDATA[ <% toUserName %> ]]></ToUserName>
+        <ToUserName><![CDATA[<%= toUserName %>]]></ToUserName>
         <FromUserName><![CDATA[ <% fromUserName %> ]]></FromUserName>
         <CreateTime> <% createTime %> </CreateTime>
-        <MsgType><![CDATA[<% msgType %>]]></MsgType>
+        <MsgType><![CDATA[<%= msgType %>]]></MsgType>
         <% if (msgType === 'test') { %>
-            <Content><![CDATA[<%- content %>]]></Content>
+            <Content><![CDATA[<%= content %>]]></Content>
         <% } else if (msgType === 'image') { %>
             <Image>
-                <MediaId><![CDATA[<% content.media_id %>]]></MediaId>
+                <MediaId><![CDATA[<%= content.media_id %>]]></MediaId>
             </Image>
         <% } else if (msgType === 'voice') { %>
             <Voice>
-                <MediaId><![CDATA[<% content.media_id %>]]></MediaId>
+                <MediaId><![CDATA[<%= content.media_id %>]]></MediaId>
             </Voice>
         <% } else if (msgType === 'video') { %>
             <Video>
-                <MediaId><![CDATA[<% content.media_id %>]]></MediaId>
-                <Title><![CDATA[<% content.title %>]]></Title>
-                <Description><![CDATA[<% content.description %>]]></Description>
+                <MediaId><![CDATA[<%= content.media_id %>]]></MediaId>
+                <Title><![CDATA[<%= content.title %>]]></Title>
+                <Description><![CDATA[<%= content.description %>]]></Description>
             </Video>
         <% } else if (msgType === 'music') { %>
             <Music>
-                <Title><![CDATA[<% content.title %>]]></Title>
-                <Description><![CDATA[<% content.description %>]]></Description>
-                <MusicUrl><![CDATA[<% content.musicUrl %>]]></MusicUrl>
-                <HQMusicUrl><![CDATA[<% content.hqMusicUrl %>]]></HQMusicUrl>
-                <ThumbMediaId><![CDATA[<% content.thumbMediaId %>]]></ThumbMediaId>
+                <Title><![CDATA[<%= content.title %>]]></Title>
+                <Description><![CDATA[<%= content.description %>]]></Description>
+                <MusicUrl><![CDATA[<%= content.musicUrl %>]]></MusicUrl>
+                <HQMusicUrl><![CDATA[<%= content.hqMusicUrl %>]]></HQMusicUrl>
+                <ThumbMediaId><![CDATA[<%= content.thumbMediaId %>]]></ThumbMediaId>
             </Music>
         <% } else if (msgType === 'news') { %>
             <ArticleCount><% content.length %></ArticleCount>
             <Articles>
                 <% content.forEach(function(item) { %>
                     <item>
-                        <Title><![CDATA[<% item.title %>]]></Title> 
-                        <Description><![CDATA[<% item.description %>]]></Description>
-                        <PicUrl><![CDATA[<% item.picurl %>]]></PicUrl>
-                        <Url><![CDATA[<% item.url %>]]></Url>
+                        <Title><![CDATA[<%= item.title %>]]></Title> 
+                        <Description><![CDATA[<%= item.description %>]]></Description>
+                        <PicUrl><![CDATA[<%= item.picurl %>]]></PicUrl>
+                        <Url><![CDATA[<%= item.url %>]]></Url>
                     </item>
                 <% }) %>
             </Articles>
